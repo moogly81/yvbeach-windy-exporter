4d071ad3c8cf9f581e4dcc296e5f1a14a1b49605
Damien Sonney <dsonney@pictet.com>
2021-06-21 16:06:56 +0200

    --no-edit

--- bash/start.sh
+++ bash/start.sh
@@ -5,6 +5,13 @@ if [ -z "$WINDY_API_KEY" ] ;  then
   echo 'echo "export WINDY_API_KEY=XXXXXXXX" > ../.setup-api-key.sh'
   echo 'source  ../.setup-api-key.sh'
 fi
+if [ -z "$WINDY_COM_API_KEY" ] ;  then 
+  echo "WINDY_API_KEY not found in environment, please export it."
+  echo "ex:"
+  echo 'echo "export WINDY_COMAPI_KEY=XXXXXXXX" >> ../.setup-api-key.sh'
+  echo 'source  ../.setup-api-key.sh'
+fi
+
 
 sudo docker build -t yvbeach-to-windy .
-sudo docker run --name ybeach-to-windy -d --env WINDY_API_KEY=${WINDY_API_KEY} yvbeach-to-windy
+sudo docker run --name ybeach-to-windy -d --env WINDY_API_KEY="${WINDY_API_KEY}" yvbeach-to-windy
